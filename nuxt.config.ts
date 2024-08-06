9ffd83b870aabc595fbf9300926f8ddb68fb9dee
thirdestonks <aguilerathirde@gmail.com>
2024-08-06 16:07:58 +0800

    update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+
+  compatibilityDate: '2024-08-06'
 })
